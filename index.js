9eee6aa43929b16c4b671701d928908a26a328bf
sarveshgupta1997 <sarveshgupta14@gmail.com>
2023-02-10 03:14:42 +0530

    backend repo

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const io = require("socket.io")(8000);
+const express= require("express");
+const http= require("http");
+const {Server} = require("socket.io");
+var cors = require('cors');
+
+const app = express();
+app.use(cors());
+const httpServer = http.createServer(app)
+
+app.get("/",(req,res)=>{
+        res.send("homepage")
+})
+
+app.get("/about",(req,res)=>{
+        res.send("aboutpage")
+})
+
+httpServer.listen(4400,()=>{console.log("Listening on 4400")})
+
+// Combining http server using express with web socket.io server
+const io = new Server(httpServer)
 
 const users = {};
 
@@ -11,6 +31,11 @@ io.on("connection", socket  =>{
 
     socket.on("send_message", message =>{
         socket.broadcast.emit("receive_message", {message, name:users[socket.id]});
+    })
+
+    socket.on("disconnect", message =>{
+        socket.broadcast.emit("left_message", users[socket.id]);
+        delete users[socket.id];
     })
 })
 
